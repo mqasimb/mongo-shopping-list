81667e2185dedc545af25527b8414f6321e4ccef
bqasim381 <bqasim381@gmail.com>
2016-11-15 01:26:10 +0000

    added mlab

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
                             'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev');
+                            'mongodb://mongoshoppinglist:abc123@ds153637.mlab.com:53637/mongo-shopping-list');
 exports.PORT = process.env.PORT || 8080;
