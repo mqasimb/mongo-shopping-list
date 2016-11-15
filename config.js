55df09497f7769a3223babd0a7165666a4d5005a
mqasimb <bqasim381@gmail.com>
2016-11-14 23:08:17 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                       (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
-                            'mongodb://mongoshoppinglist:abc123@ds153637.mlab.com:53637/mongo-shopping-list');
+exports.DATABASE_URL = 'mongodb://mongoshoppinglist:abc123@ds153637.mlab.com:53637/mongo-shopping-list';
 exports.PORT = process.env.PORT || 8080;
